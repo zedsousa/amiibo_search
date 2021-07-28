c83d4dc2c1aaade1ccbb913f0bf2847851143c41
MarcosNobrega <marcosmnobrega@gmail.com>
2021-07-28 14:15:29 -0300

    Corrigindo metodo em views

--- amiibo/views.py
+++ amiibo/views.py
@@ -7,13 +7,13 @@ def list_amiibos(request):
     return render(request, 'amiibos/amiibos_list.html', context)
 
 def list_amiibos_byCharacter(request, character):
-    context = {'amiibos_byCharacter_list': Amiibo.objects.gei(character=character)}
+    context = {'amiibos_byCharacter_list': Amiibo.objects.get(character=character)}
     return render(request, 'amiibos/amiibos_byCharacter_list.html', context)
 
 def list_amiibos_byGame(request, game):
-    context = {'amiibos_byGame_list': Amiibo.objects.gei(gameSeries=game)}
+    context = {'amiibos_byGame_list': Amiibo.objects.get(gameSeries=game)}
     return render(request, 'amiibos/amiibos_byGame_list.html', context)
 
 def list_amiibos_byName(request, name):
-    context = {'amiibos_byName_list': Amiibo.objects.gei(name=name)}
+    context = {'amiibos_byName_list': Amiibo.objects.get(name=name)}
     return render(request, 'amiibos/amiibos_byName_list.html', context)

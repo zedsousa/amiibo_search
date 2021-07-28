0631a8579c18fea5a63bb8bb718bd57cf02ddb3c
MarcosNobrega <marcosmnobrega@gmail.com>
2021-07-28 18:35:20 -0300

    Retorno dos amiibos por filtro completa

--- amiibo/views.py
+++ amiibo/views.py
@@ -7,21 +7,21 @@ def list_amiibos(request):
     return render(request, 'amiibos/amiibos_list.html', context)
 
 def list_amiibos_byId(request, id):
-    context = {'amiibos_byId_list': Amiibo.objects.get(id=id)}
+    context = {'amiibos_list': Amiibo.objects.all().get(id=id)}
     return render(request, 'amiibos/amiibos_byId_list.html', context)
 
 def list_amiibos_byCharacter(request, character):
-    context = {'amiibos_byCharacter_list': Amiibo.objects.get(character=character)}
+    context = {'amiibos_list': Amiibo.objects.all().filter(character=character)}
     return render(request, 'amiibos/amiibos_byCharacter_list.html', context)
 
 def list_amiibos_byGame(request, game):
-    context = {'amiibos_byGame_list': Amiibo.objects.get(gameSeries=game)}
+    context = {'amiibos_list': Amiibo.objects.all().filter(gameSeries=game)}
     return render(request, 'amiibos/amiibos_byGame_list.html', context)
 
 def list_amiibos_byName(request, name):
-    context = {'amiibos_byName_list': Amiibo.objects.get(name=name)}
+    context = {'amiibos_list': Amiibo.objects.all().filter(name=name)}
     return render(request, 'amiibos/amiibos_byName_list.html', context)
 
 def list_amiibos_byType(request, type):
-    context = {'amiibos_byType_list': Amiibo.objects.get(type=type)}
+    context = {'amiibos_list': Amiibo.objects.all().filter(type=type)}
     return render(request, 'amiibos/amiibos_byType_list.html', context)

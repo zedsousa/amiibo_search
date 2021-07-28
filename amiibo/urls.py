0b65a38a2e3414cd2cdfc5ed8aa2950e32488569
MarcosNobrega <marcosmnobrega@gmail.com>
2021-07-28 14:09:23 -0300

    inserindo busca de amiibo por personagem, nome e jogo

--- amiibo/urls.py
+++ amiibo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.list_amiibos)
+    path('list/', views.list_amiibos),
+    path('list/<str:character>', views.list_amiibos_byCharacter, name='list_amiibos_byCharacter'),
+    path('list/<str:game>', views.list_amiibos_byGame, name='list_amiibos_byGame'),
+    path('list/<str:name>', views.list_amiibos_byName, name='list_amiibos_byName'),
 ]
--- amiibo/views.py
+++ amiibo/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 from .models import Amiibo
 
 def list_amiibos(request):
-    context = {'amiibos_list': Amiibo.objects.all()};
-    return render(request, 'amiibos/amiibos_list.html', context)    
+    context = {'amiibos_list': Amiibo.objects.all()}
+    return render(request, 'amiibos/amiibos_list.html', context)
+
+def list_amiibos_byCharacter(request, character):
+    context = {'amiibos_byCharacter_list': Amiibo.objects.gei(character=character)}
+    return render(request, 'amiibos/amiibos_byCharacter_list.html', context)
+
+def list_amiibos_byGame(request, game):
+    context = {'amiibos_byGame_list': Amiibo.objects.gei(gameSeries=game)}
+    return render(request, 'amiibos/amiibos_byGame_list.html', context)
+
+def list_amiibos_byName(request, name):
+    context = {'amiibos_byName_list': Amiibo.objects.gei(name=name)}
+    return render(request, 'amiibos/amiibos_byName_list.html', context)

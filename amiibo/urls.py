248eacba33d94010c140fdc8abffb7a02f846403
MarcosNobrega <marcosmnobrega@gmail.com>
2021-07-28 18:01:56 -0300

    Inserindo templates para cada busca

--- amiibo/urls.py
+++ amiibo/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('list/', views.list_amiibos),
     path('list/<int:id>', views.list_amiibos_byId, name='list_amiibos_byId'),
-    path('list/<str:character>', views.list_amiibos_byCharacter, name='list_amiibos_byCharacter'),
-    path('list/<str:game>', views.list_amiibos_byGame, name='list_amiibos_byGame'),
-    path('list/<str:name>', views.list_amiibos_byName, name='list_amiibos_byName'),
-    path('list/<str:type>', views.list_amiibos_byType, name='list_amiibos_byType'),
+    path('list/?character=<str:character>', views.list_amiibos_byCharacter, name='list_amiibos_byCharacter'),
+    path('list/?game=<str:game>', views.list_amiibos_byGame, name='list_amiibos_byGame'),
+    path('list/?name=<str:name>', views.list_amiibos_byName, name='list_amiibos_byName'),
+    path('list/?type=<str:type>', views.list_amiibos_byType, name='list_amiibos_byType'),
 ]

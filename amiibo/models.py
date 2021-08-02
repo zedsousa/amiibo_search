4319f1d1338196624686f48f9d6eea25353463ab
Sousa <jose.sousa1251@gmail.com>
2021-08-02 15:50:33 -0300

    Atualizando requirements.txt

--- amiibo/models.py
+++ amiibo/models.py
@@ -10,4 +10,7 @@ class Amiibo(models.Model):
     name = models.CharField(max_length=50)
     tail = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
     
